2a742e66b59d1ce37a672e27015618e6d09d1f35
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-04 22:07:50 -0600

    Removal of vendor index action per project requirements.

--- app/controllers/api/v1/vendors_controller.rb
+++ app/controllers/api/v1/vendors_controller.rb
@@ -1,8 +1,4 @@
 class Api::V1::VendorsController < ApplicationController
-  def index 
-    render json: Vendor.all
-  end
-
   def show
     render json: Vendor.find(params[:id])
   end
--- spec/requests/api/v1/vendors_request_spec.rb
+++ spec/requests/api/v1/vendors_request_spec.rb
@@ -1,39 +1,7 @@
 require 'rails_helper'
 
 describe "Vendors API" do 
-  it 'sends a list of vendors' do 
-    create_list(:vendor, 3)
-
-    get '/api/v1/vendors'
-
-    expect(response).to be_successful
-
-    vendors = JSON.parse(response.body,symbolize_names: true)
-
-    expect(vendors.count).to eq(3)
-
-    vendors.each do |vendor| 
-      expect(vendor).to have_key(:id)
-      expect(vendor[:id]).to be_a(Integer)
-
-      expect(vendor).to have_key(:name)
-      expect(vendor[:name]).to be_a(String)
-
-      expect(vendor).to have_key(:description)
-      expect(vendor[:description]).to be_a(String) 
-
-      expect(vendor).to have_key(:contact_name)
-      expect(vendor[:contact_name]).to be_a(String)
-
-      expect(vendor).to have_key(:contact_phone)
-      expect(vendor[:contact_phone]).to be_a(String)
-
-      expect(vendor).to have_key(:credit_accepted)
-      expect(vendor[:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
-    end
-  end
-
-    it 'can render a vendor by :id' do 
+    it 'can render a single vendor by their :id' do 
       id = create(:vendor).id
 
       get "/api/v1/vendors/#{id}"
