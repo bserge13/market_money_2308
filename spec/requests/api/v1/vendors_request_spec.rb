849117adf267491fc3c37259d6fde029a72e701c
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-04 21:52:45 -0600

    Testing of vendor index action of api. All testing passes.

---
+++ spec/requests/api/v1/vendors_request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "Vendors API" do 
+  it 'sends a list of vendors' do 
+    create_list(:vendor, 3)
+
+    get '/api/v1/vendors'
+
+    expect(response).to be_successful
+
+    vendors = JSON.parse(response.body,symbolize_names: true)
+
+    expect(vendors.count).to eq(3)
+
+    vendors.each do |vendor| 
+      expect(vendor).to have_key(:id)
+      expect(vendor[:id]).to be_a(Integer)
+
+      expect(vendor).to have_key(:name)
+      expect(vendor[:name]).to be_a(String)
+
+      expect(vendor).to have_key(:description)
+      expect(vendor[:description]).to be_a(String) 
+
+      expect(vendor).to have_key(:contact_name)
+      expect(vendor[:contact_name]).to be_a(String)
+
+      expect(vendor).to have_key(:contact_phone)
+      expect(vendor[:contact_phone]).to be_a(String)
+
+      expect(vendor).to have_key(:credit_accepted)
+      expect(vendor[:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
+    end
+  end
+end
