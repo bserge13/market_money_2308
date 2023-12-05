28c38e16fbd598d1268d29d431bc25c03c6d115a
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-04 19:31:14 -0600

    validation testing complete for vendor table.

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Vendor, type: :model do
   end
 
   describe 'validations' do 
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:contact_name) }
+    it { should validate_presence_of(:contact_phone) }
+    it { should allow_value(true).for(:credit_accepted) }
+    it { should allow_value(false).for(:credit_accepted) }
 
   end
 end
