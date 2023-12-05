f6f5744c8b7de88b308369769fe1e8eb2abfc24a
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-04 23:44:31 -0600

    Show action for Market controller. All testing passes.

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -41,4 +41,42 @@ RSpec.describe 'Markets API' do
       expect(market[:lon]).to be_a(String)
     end
   end
+
+  it 'can get a single market by its id' do 
+    id = create(:market).id
+
+    get "/api/v0/markets/#{id}"
+
+    market = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(market).to have_key(:id) 
+    expect(market[:id]).to be_an(Integer) 
+
+    expect(market).to have_key(:name)
+    expect(market[:name]).to be_a(String)
+
+    expect(market).to have_key(:street)
+    expect(market[:street]).to be_a(String) 
+
+    expect(market).to have_key(:city)
+    expect(market[:city]).to be_a(String)
+
+    expect(market).to have_key(:county)
+    expect(market[:county]).to be_a(String)
+
+    expect(market).to have_key(:state)
+    expect(market[:state]).to be_a(String)
+    
+    expect(market).to have_key(:zip)
+    expect(market[:zip]).to be_a(String)
+    
+    expect(market).to have_key(:lat)
+    expect(market[:county]).to be_a(String)
+    
+    expect(market).to have_key(:lon)
+    expect(market[:lon]).to be_a(String)
+
+  end
 end
