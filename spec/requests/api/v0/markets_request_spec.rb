3c3bf0e34548a18b17642fdacd566f78697a0563
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-05 19:32:53 -0600

    Refactor testing: market index action testing for serialized endpoint. All testing passes.

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -10,35 +10,18 @@ RSpec.describe 'Markets API' do
 
     markets = JSON.parse(response.body, symbolize_names: true)
 
-    expect(markets.count).to eq(4) 
-
-    markets.each do |market| 
-      expect(market).to have_key(:id) 
-      expect(market[:id]).to be_an(Integer) 
-
-      expect(market).to have_key(:name)
-      expect(market[:name]).to be_a(String)
-
-      expect(market).to have_key(:street)
-      expect(market[:street]).to be_a(String) 
-
-      expect(market).to have_key(:city)
-      expect(market[:city]).to be_a(String)
-
-      expect(market).to have_key(:county)
-      expect(market[:county]).to be_a(String)
-
-      expect(market).to have_key(:state)
-      expect(market[:state]).to be_a(String)
-      
-      expect(market).to have_key(:zip)
-      expect(market[:zip]).to be_a(String)
-      
-      expect(market).to have_key(:lat)
-      expect(market[:county]).to be_a(String)
-      
-      expect(market).to have_key(:lon)
-      expect(market[:lon]).to be_a(String)
+    expect(markets[:data].count).to eq(4) 
+    markets[:data].each do |market| 
+      expect(market).to be_an(Hash)
+      expect(market).to have_key(:id)
+      expect(market[:attributes]).to have_key(:name)
+      expect(market[:attributes]).to have_key(:street)
+      expect(market[:attributes]).to have_key(:city)
+      expect(market[:attributes]).to have_key(:county)
+      expect(market[:attributes]).to have_key(:state)
+      expect(market[:attributes]).to have_key(:zip)
+      expect(market[:attributes]).to have_key(:lat)      
+      expect(market[:attributes]).to have_key(:lon)
     end
   end
 
@@ -51,32 +34,15 @@ RSpec.describe 'Markets API' do
 
     expect(response).to be_successful
 
-    expect(market).to have_key(:id) 
-    expect(market[:id]).to be_an(Integer) 
-
-    expect(market).to have_key(:name)
-    expect(market[:name]).to be_a(String)
-
-    expect(market).to have_key(:street)
-    expect(market[:street]).to be_a(String) 
-
-    expect(market).to have_key(:city)
-    expect(market[:city]).to be_a(String)
-
-    expect(market).to have_key(:county)
-    expect(market[:county]).to be_a(String)
-
-    expect(market).to have_key(:state)
-    expect(market[:state]).to be_a(String)
-    
-    expect(market).to have_key(:zip)
-    expect(market[:zip]).to be_a(String)
-    
-    expect(market).to have_key(:lat)
-    expect(market[:county]).to be_a(String)
-    
-    expect(market).to have_key(:lon)
-    expect(market[:lon]).to be_a(String)
+    expect(market[:data]).to have_key(:id) 
+    expect(market[:data]).to have_key(:name)
+    expect(market[:data]).to have_key(:street)
+    expect(market[:data]).to have_key(:city)
+    expect(market[:data]).to have_key(:county)
+    expect(market[:data]).to have_key(:state)
+    expect(market[:data]).to have_key(:zip)
+    expect(market[:data]).to have_key(:lat)
+    expect(market[:data]).to have_key(:lon)
   end
 
   it 'can get all vendors for a given market' do 
